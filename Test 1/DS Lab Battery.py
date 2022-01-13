d4b49ace5752e60af8359222d636dae5da92baf1
Ali Ashber <k201051@nu.edu.pk>
2022-01-14 00:59:28 +0500

    Add project files.

---
+++ Test 1/DS Lab Battery.py
@@ -0,0 +1,48 @@
+# importing csv module
+import csv
+import networkx as nx
+import matplotlib.pyplot as plt
+
+
+
+class GraphVisualization:
+   
+    def __init__(self):
+        self.visual = []
+   
+    def addEdge(self, a, b):
+        temp = [a, b]
+        self.visual.append(temp)
+          
+
+    def visualize(self):
+        G = nx.Graph()
+        G.add_edges_from(self.visual)
+        nx.draw_networkx(G)
+        plt.show()
+  
+
+G = GraphVisualization()
+
+filename = "test.csv"
+  
+
+  
+
+with open(filename, 'r') as csvfile:
+    csvreader = csv.reader(csvfile)
+    fields = next(csvreader)
+  
+
+    for row in csvreader:
+       source = row[2]
+       for x in range(10,30,2):
+           if row[x]!='0':
+               G.addEdge(source,row[x])
+
+
+            
+
+
+
+    G.visualize()
