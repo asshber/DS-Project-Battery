3ff3b7409a062a12b0743b9a81dffc4d4153a0fc
Ali Ashber <k201051@nu.edu.pk>
2022-01-14 15:43:12 +0500

    gg ez

--- Test 1/DS Lab Battery.py
+++ Test 1/DS Lab Battery.py
@@ -2,7 +2,7 @@ import csv
 import networkx as nx
 import numpy as np
 import matplotlib.pyplot as plt
-
+from collections import OrderedDict
 
 class GraphVisualization:
    
@@ -23,10 +23,9 @@ class GraphVisualization:
 
 G = GraphVisualization()
 
-filename = "test.csv"
+filename = "Input.csv"
   
-arr=np.array([0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0])
-  
+dic = {1:255,2:255,3:255,4:255,5:255,6:255,7:255,8:255,9:255,10:255,11:255,12:255,13:255,14:255,15:255,16:255}
 
 with open(filename, 'r') as csvfile:
     csvreader = csv.reader(csvfile)
@@ -36,17 +35,25 @@ with open(filename, 'r') as csvfile:
     for row in csvreader:
        source = row[2]
        index=int(source)
-       arr[index-1]=row[8]
+       if(int(dic[index])>int(row[8])):
+         dic[index]=int(row[8])
        for x in range(10,31,2):
            if row[x]!='0':
                index=int(row[x])
-               arr[index-1]=int(row[x+1])
+               if(int(dic[index])>int(row[x+1])):
+                 dic[index]=int(row[x+1])
                G.addEdge(source,row[x])
     G.visualize()
-    print(arr)
+    dic=sorted(dic.items(), key = lambda kv:(kv[1], kv[0]))
+    print(dic)   
 
-            
+header=["Vertices", "battery"]
+with open('Output.csv', 'w') as f:
+    writer = csv.writer(f)
+
+    writer.writerow(header)
+    for key, value in dic:
+        writer.writerow([key, value])
 
 
-
-    
+    f.close()
